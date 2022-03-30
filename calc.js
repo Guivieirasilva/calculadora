20d94c5c93ce0f3d1bcc065a6bd1ff107f0f07fc
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-03-30 12:03:54 -0300

    calculadora

--- calc.js
+++ calc.js
@@ -1,3 +1,10 @@
 function botao(num) {
-    
+    valor = document.calc.visor.value += num
+}
+function reset(){
+    document.calc.visor.value = ""
+}
+function calcular(){
+    resultado = eval(valor)
+    document.calc.visor.value = resultado.toLocaleString('pt-br')
 }
